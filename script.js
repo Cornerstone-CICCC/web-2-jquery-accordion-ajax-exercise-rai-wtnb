4042ffa26786fd239e547677c7a80a0541307026
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-11-26 21:17:33 -0800

    chore: finish

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-$(function() {
-  // your code here
-})
+$(function () {
+  // exercise 1
+  $(".accordion-header").click(function () {
+    $(this).next(".accordion-content").slideToggle();
+  });
+
+  // exercise 2
+  $("button").click(function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        data.todos.map((todo) => {
+          $(".todos ul").append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function (error) {
+        $(".todos ul").append(`<li style="color: red;">Error occurred</li>`);
+      },
+    });
+  });
+});
